80e09358924f8d4b2ab6852dda9c3f9fce5baf93
sanyachokedee <ing11819@gmail.com>
2024-03-12 14:12:09 +0700

    first

---
+++ src/main/kotlin/com/csthai/productservice/models/Product.kt
@@ -0,0 +1,31 @@
+package com.csthai.productservice.models
+
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+@Entity
+@Table(name= "products")
+data class Product(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    val id: Int =0,
+
+    @Column(name= "product_name" , nullable= false, length =255)
+    val productName: String,
+
+    @Column(name= "product_price", nullable= false, precision =10 , scale= 2)
+    val productPrice: BigDecimal = BigDecimal.ZERO,
+
+    @Column(name = "product_description")
+    val  productDescription: String? = null,
+
+    @Column(name= "product_quantity" , nullable=false)
+    val productQuantity: Int =0,
+
+    @Column(name= "created_at", nullable=false)
+    val createdAt : LocalDateTime = LocalDateTime.now(),
+
+    )
