44de286675ed43449037ab0cdd381d207eaab5f5
sanyachokedee <ing11819@gmail.com>
2024-03-12 14:09:10 +0700

    first commit

---
+++ src/main/kotlin/com/csthai/productservice/SpringProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.csthai.productservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringProductServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringProductServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/csthai/productservice/SpringProductServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.csthai.productservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringProductServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
